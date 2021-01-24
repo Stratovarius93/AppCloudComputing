0ad10273efda61f70e9334f7104e17076e2afffe
stratovarius93 <www.warcry@hotmail.es>
2021-01-23 22:56:03 -0500

    Pull

--- lib/requests/FirebaseRequest.dart
+++ lib/requests/FirebaseRequest.dart
@@ -7,24 +7,20 @@ class FirebaseRequest {
   String url = 'https://apptodo-7c8d1-default-rtdb.firebaseio.com/tasks.json';
 
   Future<bool> fetchPush(Task task) async {
-  final res = await http.post(url, body: taskToJson(task));
-  return res.statusCode == 200;
+    final res = await http.post(url, body: taskToJson(task));
+    return res.statusCode == 200;
   }
 
   Future<List<Task>> fetchGet() async {
     final res = await http.get(url);
     if (res.statusCode == 200) {
-      //final decodeRes = jsonDecode(res.body);
       final decodeRes = json.decode(res.body);
       List<Task> list = [];
-      //final data = decodeRes.keys.map((item) => decodeRes[item]).toList();
-      //return data;
       decodeRes.forEach((id, task) {
         final tasks = Task.fromJson(task);
         list.add(tasks);
       });
       return list;
-      //print(data1);
     } else {
       throw 'Error ';
     }
