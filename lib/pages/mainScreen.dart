fb5c0dba6ecac66509d4b54e06505622f5e7ce62
stratovarius93 <www.warcry@hotmail.es>
2021-01-23 21:19:01 -0500

    Al fin vel el GET

--- lib/pages/mainScreen.dart
+++ lib/pages/mainScreen.dart
@@ -1,3 +1,4 @@
+import 'package:AppCloudComputing/model/taskModel.dart';
 import 'package:AppCloudComputing/provider/taskProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,20 +9,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final taskProvider = Provider.of<TaskProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('App notas'),
       ),
       body: ListView.builder(
-        itemCount: list.length,
+        itemCount: taskProvider.getTaskList.length,
         itemBuilder: (BuildContext context, int index) {
-          //if (taskProvider.getTaskList.length != 0) {
+          if (taskProvider.getTaskList.length != 0) {
             return ListTile(
-              title: Text(list[index]),
+              title: Text(taskProvider.getTaskList[index].title),
+              subtitle: Text(taskProvider.getTaskList[index].description),
             );
-          //} else {
-            //return CircularProgressIndicator();
-          //}
+          } else {
+            return CircularProgressIndicator();
+          }
         },
       ),
       floatingActionButton: FloatingActionButton(
--- lib/requests/FirebaseRequest.dart
+++ lib/requests/FirebaseRequest.dart
@@ -6,21 +6,24 @@ import 'package:http/http.dart' as http;
 class FirebaseRequest {
   String url = 'https://apptodo-7c8d1-default-rtdb.firebaseio.com/tasks.json';
 
-  //Future<bool> fetchPush(Task task) async {
-  //final res = await http.post(url, body: taskToJson(task));
-  //return res.statusCode == 200;
-  //return  false;
-  //}
+  Future<bool> fetchPush(Task task) async {
+  final res = await http.post(url, body: taskToJson(task));
+  return res.statusCode == 200;
+  }
 
   Future<List<Task>> fetchGet() async {
     final res = await http.get(url);
     if (res.statusCode == 200) {
-      final decodeRes = jsonDecode(res.body);
-      //final data1 = decodeRes.keys.map((item) => decodeRes[item]).toList();
-      final data1 = decodeRes.keys.map((item) => decodeRes[item]).toList();
-      print(data1);
-
-      return data1;
+      //final decodeRes = jsonDecode(res.body);
+      final decodeRes = json.decode(res.body);
+      List<Task> list = [];
+      //final data = decodeRes.keys.map((item) => decodeRes[item]).toList();
+      //return data;
+      decodeRes.forEach((id, task) {
+        final tasks = Task.fromJson(task);
+        list.add(tasks);
+      });
+      return list;
       //print(data1);
     } else {
       throw 'Error ';
